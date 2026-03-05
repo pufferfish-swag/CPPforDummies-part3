4754272e5dbd23a10680f4894c578b6d0ec11035
PCSTAIRWAY-ADJI\adjie <adjie9876@gmail.com>
2026-03-05 19:29:28 +0700

    add class character function

--- main.cpp
+++ main.cpp
@@ -12,9 +12,22 @@ void draw_worldMap(Texture2D worldMap, Vector2 mapPos){
 class character
 {
     public:
-        //data
+        Vector2 getWorldPos(){
+            return worldPos;
+        }
     private:
-        /* data */;
+        Texture2D texture;
+        Texture2D idle;
+        Texture2D run;
+        Vector2 screenPos;
+        Vector2 worldPos;
+        //1: facing right, -1: facing left
+        float rightLeft{1.f};
+        //anim variables
+        float runningTime{};
+        int frame{};
+        const int maxFrames{6};
+        const float updateTime{1.f/12.f}; 
 };
 
 
