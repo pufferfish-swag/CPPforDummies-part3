9ca05703eb34c820bf0933604a44b4d686599b9c
COLORFUL-ADJIE\Adjie <adjie9876@gmail.com>
2026-03-13 19:42:33 +0700

    add prop class cpp and h

---
+++ prop.cpp
@@ -0,0 +1,8 @@
+#include "prop.h"
+//prop member initialize list
+Prop::Prop(Vector2 pos, Texture2D tex):
+    worldPos(pos),
+    texture(tex)
+{
+
+}
---
+++ prop.h
@@ -0,0 +1,10 @@
+#include "raylib.h"
+
+class Prop{
+    public:
+        Prop(Vector2 pos, Texture2D tex);
+    private:
+        Texture2D texture{};
+        Vector2 worldPos{};
+        float scale{4.f};
+};
